b983e2cd03daaec6b954cdecb06f11b82f1ebfbe
xzj-macair <feiyun260@qq.com>
2024-10-20 20:51:56 +0800

    初始化

---
+++ src/main/index.js
@@ -0,0 +1,75 @@
+import { app, shell, BrowserWindow, ipcMain } from 'electron'
+import { join } from 'path'
+import { electronApp, optimizer, is } from '@electron-toolkit/utils'
+import icon from '../../resources/icon.png?asset'
+
+function createWindow() {
+  // 创建浏览器窗口。
+  const mainWindow = new BrowserWindow({
+    width: 900,
+    height: 670,
+    show: false,
+    autoHideMenuBar: true,
+    ...(process.platform === 'linux' ? { icon } : {}),
+    webPreferences: {
+      preload: join(__dirname, '../preload/index.js'),
+      sandbox: false
+    }
+  })
+
+  // 默认开启开发者工具
+  mainWindow.webContents.openDevTools()
+
+  mainWindow.on('ready-to-show', () => {
+    mainWindow.show()
+  })
+
+  mainWindow.webContents.setWindowOpenHandler((details) => {
+    shell.openExternal(details.url)
+    return { action: 'deny' }
+  })
+
+  // 基于 electron-vite CLI 的渲染器热重载。
+  // 开发时加载远程 URL，生产时加载本地 HTML 文件。
+  if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
+    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
+  } else {
+    mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
+  }
+}
+
+// 当 Electron 完成初始化并准备好创建浏览器窗口时调用此方法。
+// 某些 API 只有在此事件发生后才能使用。
+app.whenReady().then(() => {
+  // 为 Windows 设置应用程序用户模型 ID
+  electronApp.setAppUserModelId('com.electron')
+
+  // 默认在开发时通过 F12 打开或关闭开发者工具，
+  // 在生产环境中忽略 CommandOrControl + R。
+  // 参见 https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
+  app.on('browser-window-created', (_, window) => {
+    optimizer.watchWindowShortcuts(window)
+  })
+
+  // IPC 测试
+  ipcMain.on('ping', () => console.log('pong'))
+
+  createWindow()
+
+  app.on('activate', function () {
+    // 在 macOS 上，当点击 dock 图标且没有其他窗口打开时，
+    // 通常会重新创建一个窗口。
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// 当所有窗口都关闭时退出应用程序，除了在 macOS 上。
+// 在 macOS 上，应用程序及其菜单栏通常会保持活动状态，直到用户显式使用 Cmd + Q 退出。
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+// 在此文件中可以包含应用程序特定的主进程代码。
+// 也可以将它们放在单独的文件中并在这里引用。
